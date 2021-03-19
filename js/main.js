0eacb26c16e768179783afc0d1a07f4f9396664d
himanshsoni <himanshsoni5@gmail.com>
2021-03-19 19:19:49 +0530

    Added Title

--- js/main.js
+++ js/main.js
@@ -3,15 +3,21 @@ showNotes();
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function (e) {
   let addTxt = document.getElementById("addTxt");
+  let addTitle = document.getElementById("addTitle");
   let notes = localStorage.getItem("notes");
   if (notes == null) {
     notesObj = [];
   } else {
     notesObj = JSON.parse(notes);
   }
-  notesObj.push(addTxt.value);
+  let Obj = {
+    title: addTitle.value,
+    text: addTxt.value,
+  };
+  notesObj.push(Obj);
   localStorage.setItem("notes", JSON.stringify(notesObj));
   addTxt.value = "";
+  addTitle.value="";
   console.log(notesObj);
   showNotes();
 });
@@ -22,26 +28,24 @@ function showNotes() {
   } else {
     notesObj = JSON.parse(notes);
   }
-  let html=""
-  notesObj.forEach(function (element,index) {   
+  let html = "";
+  notesObj.forEach(function (element, index) {
     html += `
         <div class="noteCard mx-2 my-2" style="width: 18rem;">
           <div class="card-body">
-            <h5 class="card-title">Note ${index+1}</h5>
-            <p class="card-text"> ${element}</p>
+            <h5 class="card-title"> ${element.title}</h5>
+            <p class="card-text"> ${element.text}</p>
             <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary"> Delete Note</button>
           </div>
       </div>
     `;
   });
-  let notesElm=document.getElementById('notes')
-  if (notesObj.length!=0) {
-     notesElm.innerHTML=html 
+  let notesElm = document.getElementById("notes");
+  if (notesObj.length != 0) {
+    notesElm.innerHTML = html;
+  } else {
+    notesElm.innerHTML = `Nothing to Show`;
   }
-  else{
-    notesElm.innerHTML=`Nothing to Show`
-  }
-  
 }
 // functon to delete Node
 function deleteNote(index) {
@@ -51,27 +55,22 @@ function deleteNote(index) {
   } else {
     notesObj = JSON.parse(notes);
   }
-  notesObj.splice(index,1)
-  localStorage.setItem("notes",JSON.stringify(notesObj))
-  showNotes()
+  notesObj.splice(index, 1);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  showNotes();
 }
 
-
 // to search
-let search=document.getElementById('searchTxt')
-search.addEventListener('input',function() {
-    let inputTxt=search.value;
-    let noteCard=document.getElementsByClassName('noteCard')
-    Array.from(noteCard).forEach(function (element) {
-        let cardTxt=element.getElementsByTagName("p")[0].innerText
-        if (cardTxt.includes(inputTxt)) {
-            element.style.display='block'
-            
-        }
-        else{
-            element.style.display='none'
-            
-        }
-
-    })
-})
+let search = document.getElementById("searchTxt");
+search.addEventListener("input", function () {
+  let inputTxt = search.value;
+  let noteCard = document.getElementsByClassName("noteCard");
+  Array.from(noteCard).forEach(function (element) {
+    let cardTxt = element.getElementsByTagName("p")[0].innerText;
+    if (cardTxt.includes(inputTxt)) {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+  });
+});
